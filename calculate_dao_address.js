00cd3dd63f0a1de6257be9758dcff0b64f8d95a7
Kuan-Hao Chen <35591407+nghdavid@users.noreply.github.com>
2024-08-08 08:02:13 +0800

    feat: Finish tesr version

---
+++ calculate_dao_address.js
@@ -0,0 +1,46 @@
+const ethers = require('ethers');
+const fs = require('fs').promises;
+require('dotenv').config();
+
+const fundraiseName = "fundraise";
+const addressVote = "0x9A78475BE1412bC735d940dbf6A7270367cAa226";
+const addressTimeLock = "0x0FaF2F23647AD8FAA94aCe13635Df22A48A34A90";
+
+async function readTextFile(filePath) {
+  try {
+    let text = await fs.readFile(filePath, 'utf8');
+    text = text.replace(/\n/g, '');
+    return text;
+  } catch (error) {
+    console.error("Error reading the file:", error);
+    throw error;
+  }
+}
+async function calculateDaoAddr(timelock, voteToken, daoName) {
+  const salt = ethers.keccak256(
+    ethers.AbiCoder.defaultAbiCoder().encode(
+      ['address', 'address'],
+      [timelock, voteToken]
+    )
+  );
+
+  const constructorArgs = ethers.AbiCoder.defaultAbiCoder().encode(
+    ['address', 'address', 'string'],
+    [voteToken, timelock, daoName]
+  );
+  const bytecodeDao = await readTextFile('dao.txt');
+  const bytecode = ethers.concat([bytecodeDao, constructorArgs]);
+
+  const predictedAddress = ethers.getCreateAddress({
+    from: ethers.ZeroAddress,
+    nonce: salt
+  });
+
+  return predictedAddress;
+}
+
+async function main() {
+  const daoAddress = await calculateDaoAddr(addressTimeLock, addressVote, fundraiseName);
+  console.log(`DAO Address: ${daoAddress}`);
+  }
+main();
--- create_treasury.js
+++ create_treasury.js
@@ -10,18 +10,17 @@ const wallet = new ethers.Wallet(privateKey, provider);
 const abiTreasuryFactory = [
   "function createTreasury(string, address, address, address, address, uint32, uint32) public returns (address)",
   "function getDAOInfo(address, address) public returns (DAOInfo[])",
-  "calculateTreasuryAddr(address, address, address) public returns (address)"
+  "function calculateTreasuryAddr(address, address, address) public returns (address)"
 ];
 
-const addressTreasuryFactory = "0xC0218aC712f49871CfDf875eB773a422D48B7947";
+const addressTreasuryFactory = "0x73cCC25c8f13e18B81059B3e39a11aE04dd8B382";
 const fundraiseName = "fundraise";
-const addressFundToken = "0x9A78475BE1412bC735d940dbf6A7270367cAa226";
-
+const addressFundToken = "0x022A71b546560C2b28732333Bcd434e555426ECF";
 const addressVoteToken = "0x9A78475BE1412bC735d940dbf6A7270367cAa226";
 const addressTimeLock = "0x0FaF2F23647AD8FAA94aCe13635Df22A48A34A90";
-const addressCompany = "0x0FaF2F23647AD8FAA94aCe13635Df22A48A34A90";
-const duration = 10;
-const fundraiseTime = 10;
+const addressCompany = "0x8e33a24CD96a0dbA80E57DD2f0D3c70338Ab046C";
+const fundraiseTime = 1000;
+const duration = 100;
 
 async function main() {
   try {
